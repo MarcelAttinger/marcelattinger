cf92be5e7cce294486797811b866a20138601a17
mattinger <marcel.attinger@udg.de>
2018-08-27 01:53:02 +0200

    Initial Commit

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Navbar from '../components/Navbar'
-import './all.sass'
+import Navbar from '../components/Navbar';
+import './all.sass';
 
 const TemplateWrapper = ({ children }) => (
   <div>
